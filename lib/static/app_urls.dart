97d0a8462b43cfb86a2e47318fe8827078e0573f
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2025-01-18 01:18:45 +0200

    update app app_urls

--- lib/static/app_urls.dart
+++ lib/static/app_urls.dart
@@ -1,22 +1,14 @@
 class AppUrls {
-<<<<<<< HEAD
   // static const String baseUrl ='http://127.0.0.1:8000/';
   static const String baseUrl = 'http://192.168.139.73:8000';
   static const String api = '/api/';
   static String getProductImage(String imageUrl) =>
       '$baseUrl/storage/$imageUrl';
   static String searchProducts(String name) => '$baseUrl/api/search/$name';
-=======
-  // Base URLs
-  static const String baseUrl = 'http://192.168.0.246:3000';
-  static const String api = '/api/';
+
   static const String apiBase = '$baseUrl$api';
 
-  // Helper methods
-  static String getProductImage(String imageUrl) => '$baseUrl/storage/$imageUrl';
-
   // Categories
->>>>>>> 65b6a10d8e43a81cb3b040458620f180c0787276
   static const String categories = '$baseUrl${api}categories';
 
   // Authentication endpoints
@@ -26,7 +18,7 @@ class AppUrls {
   static const String logout = '$auth/logout';
   static const String verifyEmail = '$auth/verify-email';
   static const String verifyOtp = '$auth/verify-otp';
-  
+
   // Social Authentication
   static const String googleAuth = '$auth/google';
   static const String googleCallback = '$auth/google/callback';
